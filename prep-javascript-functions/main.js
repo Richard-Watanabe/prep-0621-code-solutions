1009170d23c1bc9512ea4e719a0e95a09325eb51
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-06-15 20:48:26 -0700

    Defined addTwoNumberss Function

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+function addTwoNumberss(num1, num2) {
+  return num1 + num2;
+}
+
+var sum2 = addTwoNumberss(2, 2);
+console.log(sum2);
