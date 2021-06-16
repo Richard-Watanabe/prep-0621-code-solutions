e7dfda44957ddd99ea11424444e147d86dc6dab1
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-06-15 20:57:48 -0700

    Defined convertHoursToMinutes and getGreeting function

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -11,3 +11,17 @@ function addTwoNumberss(num1, num2) {
 
 var sum2 = addTwoNumberss(2, 2);
 console.log(sum2);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var minutes = convertHoursToMinutes(2);
+console.log(minutes);
+
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+var greeting = getGreeting('Tim');
+console.log(greeting);
